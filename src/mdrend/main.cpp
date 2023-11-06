867823439f61efdc22500b5fb5dd30bb6dea059c
Elias Olsson <dataolsson@gmail.com>
2023-11-06 08:01:15 +0100

    Added GLFW, GLEW, glm, stb, Engone

---
+++ src/mdrend/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#include "stb/stb_image_write.h"
+#undef STB_IMAGE_WRITE_IMPLEMENTATION
+#undef STB_IMAGE_IMPLEMENTATION
+
+int main() {
+    // using namespace engone;
+    // engone::log::out.enableReport(false);
+    
+    
+    
+    return 0;
+}
